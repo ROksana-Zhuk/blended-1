d4286d8f856d3a112519f54a87fc91c57b4991d6
ROksana-Zhuk <roksi.sun.forever@gmail.com>
2025-04-20 21:52:17 +0300

    blended-1 done

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,11 +1,47 @@
-// import { FaRegThumbsUp } from 'react-icons/fa';
-// import { MdPeople, MdOutlineProductionQuantityLimits } from 'react-icons/md';
-// import { GiTreeDoor } from 'react-icons/gi';
-// import  StatisticsItem  from '../StatisticsItem/StatisticsItem';
-// import style from './Statistics.module.css';
+import { FaRegThumbsUp } from 'react-icons/fa';
+import { MdPeople, MdOutlineProductionQuantityLimits } from 'react-icons/md';
+import { GiTreeDoor } from 'react-icons/gi';
 
-const Statistics = () => {
-  return <h2>Statistics</h2>;
+import style from './Statistics.module.css';
+import StatisticsItem from '../StatisticsItem/StatisticsItem';
+
+const icons=[
+    FaRegThumbsUp,
+    MdPeople,
+    MdOutlineProductionQuantityLimits,
+    GiTreeDoor
+];
+
+const Statistics = ({ title, stats }) => {
+  return (
+    <div>
+    {title && (
+        <h3 className={style.title}>{title}</h3>
+        )
+    }
+
+      <ul className={style.list}>
+       {stats.map((stat, index) => (
+        <li key={stat.id} className={style.item}>
+          <StatisticsItem title={stat.title}
+                          total={stat.total}
+                          icon={icons[index]}/>
+        </li>
+       ))}
+      </ul>
+    </div>
+  )
 };
 
 export default Statistics;
+
+/*
+    <li key={friend.id}
+    className={
+        clsx(
+            css.friendListAnimals
+        )
+    }>
+      <FriendListItem info={friend} />
+    </li>
+  ))} */
--- src/components/StatisticsItem/StatisticsItem.jsx
+++ src/components/StatisticsItem/StatisticsItem.jsx
@@ -1,5 +1,19 @@
-const StatisticsItem = () => {
-  return <h3>StatisticsItem</h3>;
+
+import style from './StatisticsItem.module.css'
+
+
+import stats from '../../data/stats.json';
+
+
+
+const StatisticsItem = ({ total, title, icon : Icon }) => {
+  return (
+    <>
+      <Icon size={30}/>
+      <span className={style.counter}>{total}</span>
+      <p className={style.text}>{title}</p>
+    </>
+  )
 };
 
 export default StatisticsItem;
